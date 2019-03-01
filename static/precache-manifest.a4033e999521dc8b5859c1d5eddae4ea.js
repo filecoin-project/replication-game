67606db138e4e7eea7d5815929f96a3f269e0a7a
Alan Shaw <alan.shaw@protocol.ai>
2019-03-01 17:28:01 +0000

    feat: add leaderboards grouped by params

---
+++ static/precache-manifest.a4033e999521dc8b5859c1d5eddae4ea.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "09b6c35c9b138a99e92e",
+    "url": "./static/css/main.88adb18f.chunk.css"
+  },
+  {
+    "revision": "09b6c35c9b138a99e92e",
+    "url": "./static/js/main.09b6c35c.chunk.js"
+  },
+  {
+    "revision": "65e885bbb887c17f8b21",
+    "url": "./static/css/1.90a3a6b6.chunk.css"
+  },
+  {
+    "revision": "65e885bbb887c17f8b21",
+    "url": "./static/js/1.65e885bb.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "346c3e5d773ff1e6c3f5fb9b7b866cd5",
+    "url": "./static/media/filecoin-logo.346c3e5d.svg"
+  },
+  {
+    "revision": "c47082eff9887b341ff40a8957d9a7c1",
+    "url": "./static/media/gold.c47082ef.png"
+  },
+  {
+    "revision": "f6a621b7e5aaf6968b6100f8db816d30",
+    "url": "./static/media/silver.f6a621b7.png"
+  },
+  {
+    "revision": "486ada9af0f88113d13d1a7e1f81fc71",
+    "url": "./static/media/bronze.486ada9a.png"
+  },
+  {
+    "revision": "86fe424e92430b1793af90658e9d0306",
+    "url": "./static/media/gold-ribbon.86fe424e.png"
+  },
+  {
+    "revision": "e694d2b3c8bd85eccb00dd5feb39d781",
+    "url": "./static/media/silver-ribbon.e694d2b3.png"
+  },
+  {
+    "revision": "191e8a6b022a74cc671cb56439d5d36f",
+    "url": "./static/media/bronze-ribbon.191e8a6b.png"
+  },
+  {
+    "revision": "9d6b5bb29b5cd1902e6ec7ab4bd37250",
+    "url": "./static/media/proof-DrgPoRep.9d6b5bb2.svg"
+  },
+  {
+    "revision": "6e939b4ecb2264a3ea4cba522629a21f",
+    "url": "./static/media/proof-Zigzag.6e939b4e.svg"
+  },
+  {
+    "revision": "d46fcfb940b941855138b2cf7d059dab",
+    "url": "./static/media/proof-Unknown.d46fcfb9.svg"
+  },
+  {
+    "revision": "ee3ac7e480c3c83887f3b50d02656dc0",
+    "url": "./static/media/Montserrat-Light.ee3ac7e4.woff2"
+  },
+  {
+    "revision": "515eeaf8b22c193a82e30586b2fda0ae",
+    "url": "./static/media/Montserrat-LightItalic.515eeaf8.woff2"
+  },
+  {
+    "revision": "c38a9d8c4a647eadd860c2893c682d2b",
+    "url": "./static/media/Montserrat-Regular.c38a9d8c.woff2"
+  },
+  {
+    "revision": "940e32eb055d675e57c51f3b6879fdd7",
+    "url": "./static/media/Montserrat-Italic.940e32eb.woff2"
+  },
+  {
+    "revision": "b4f9714141878f10bbf0b8ab6e143717",
+    "url": "./static/media/Montserrat-SemiBold.b4f97141.woff2"
+  },
+  {
+    "revision": "48d9a2cc39bc60ea3f87e8348b2a17e1",
+    "url": "./static/media/Montserrat-Bold.48d9a2cc.woff2"
+  },
+  {
+    "revision": "9282e5ff8bdd6ef6d861363d0a8e5d7c",
+    "url": "./static/media/Montserrat-SemiBoldItalic.9282e5ff.woff2"
+  },
+  {
+    "revision": "4e7ad891b89cc53655a7b2b4845fab43",
+    "url": "./static/media/Montserrat-BoldItalic.4e7ad891.woff2"
+  },
+  {
+    "revision": "2ac4e7b8520cba23b08618aed264b056",
+    "url": "./static/media/Inter-UI-Regular.2ac4e7b8.woff2"
+  },
+  {
+    "revision": "c72d8321b8fcfde8a1ee10c2aa58a2e7",
+    "url": "./static/media/Inter-UI-Bold.c72d8321.woff2"
+  },
+  {
+    "revision": "0159e50e63f10ef639c6211d80cd2e89",
+    "url": "./static/media/Inter-UI-Medium.0159e50e.woff2"
+  },
+  {
+    "revision": "33d3b6eed8fac5511b01b36a50464334",
+    "url": "./static/media/Inter-UI-Italic.33d3b6ee.woff2"
+  },
+  {
+    "revision": "fb221ed44be9484b9ae41ef16347da7c",
+    "url": "./static/media/Inter-UI-BoldItalic.fb221ed4.woff2"
+  },
+  {
+    "revision": "786fa10d111e2e550bd6a8cf01fe2e2b",
+    "url": "./static/media/Inter-UI-MediumItalic.786fa10d.woff2"
+  },
+  {
+    "revision": "c5e4ea703467eb82c64c711762421ad2",
+    "url": "./index.html"
+  }
+];
--- static/service-worker.js
+++ static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46632b7ad0a1866053bad1f2b2b5dca6.js"
+  "./precache-manifest.a4033e999521dc8b5859c1d5eddae4ea.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
